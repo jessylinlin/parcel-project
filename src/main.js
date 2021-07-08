597345f3364839ae5d264b8e0902eaff45685dd3
zhanglin <1501103995@qq.com>
2021-07-08 23:17:23 +0800

    parcel

--- src/main.js
+++ src/main.js
@@ -1,12 +1,21 @@
 import foo from "./foo";
-import $ from 'jquery'
+// import $ from 'jquery'
+import './main.css'
+import logo from './3.jpg'
 foo.bar()
 
+import('jquery').then($ => {
+    $(document.body).append('<h2>121111111111113</h2>')
+})
+
 $("body").html("123")
+const image = new Image()
+image.src = logo
+document.body.append(image)
 
 if (module.hot) {
     //webpack中 accept接收两个参数，目标和回调
-    //accept 只接受一个回调函数
+    //accept 只接受一个回调函数  
     module.hot.accept(() => {
         console.log('hmr')
     })
